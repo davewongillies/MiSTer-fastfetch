65184f6ce84e127bf6c7f3a9345264714cfb873f
David Gillies <dave.gillies@gmail.com>
2026-09-15 14:12:49 +1000

    Create symlink in /etc/profile.d

--- Scripts/fastfetch.sh
+++ Scripts/fastfetch.sh
@@ -2,6 +2,10 @@
 
 CONFIG_PATH=/media/fat/Scripts/.config/fastfetch
 
+if ! [ -L "/etc/profile.d/fastfetch.sh" ]; then
+  ln -s /media/fat/Scripts/fastfetch.sh /etc/profile.d/fastfetch.sh
+fi
+
 if ! [ -f "${CONFIG_PATH}/mister.jsonc" ]; then
   cp "${CONFIG_PATH}/mister_stock.jsonc" "${CONFIG_PATH}/mister.jsonc"
 fi
